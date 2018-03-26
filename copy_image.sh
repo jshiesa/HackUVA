2c25521cab173ac82b565b09bbddcb2178819cf4
Joshua SA <jshiesa@gmail.com>
2018-03-25 21:50:12 -0400

    Deleted copy_image_scrap.sh scrap code

--- copy_image.sh
+++ copy_image.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/sh
-# start "" "C:\Program Files\Git\bin\sh.exe" --login
 echo "Pre test"
 scp pi@192.168.43.144:/home/pi/HackUVA/images/image.jpg ~/Code/HackUVA/images/image.jpg
 echo "Post test"
