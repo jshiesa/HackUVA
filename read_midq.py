e1d784330d14b3cf30b34d027fd4bb5e6d67a843
Joshua SA <jshiesa@gmail.com>
2018-03-25 22:10:55 -0400

    Cleaning up code & files

--- read_midq.py
+++ read_midq.py
@@ -5,7 +5,6 @@ access_key = "AKIAIZHM4QZ2PYAPVE6Q"
 access_secret = "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"
 region = "us-east-1"
 middle_queue = "https://sqs.us-east-1.amazonaws.com/083630338242/DeepSightMid"
-return_queue = "https://sqs.us-east-1.amazonaws.com/083630338242/DeepSightReturns"
 
 sqs = boto3.client('sqs', aws_access_key_id=access_key, aws_secret_access_key=access_secret, region_name=region)
 
@@ -16,7 +15,6 @@ while True:
                                     AttributeNames=['All'])['Attributes']['ApproximateNumberOfMessages']) > 0:
         response = sqs.receive_message(QueueUrl=middle_queue, AttributeNames=['SentTimestamp'], MaxNumberOfMessages=1,
                                        MessageAttributeNames=['All'], VisibilityTimeout=0, WaitTimeSeconds=0)
-
         print(response)
 
         message = response['Messages'][0]
@@ -30,6 +28,7 @@ while True:
         )
         print('Received and deleted message: %s' % body)
 
-        # run shell script
+        # run shell script to retrieve image from Pi
         call(["C:\\Program Files\\Git\\git-bash.exe", "copy_image.sh"])
+        # classify image and return
         call(["python", "return_response.py"])
--- return_response.py
+++ return_response.py
@@ -1,11 +1,9 @@
 import boto3
-import os
 import model
 
 access_key = "AKIAIZHM4QZ2PYAPVE6Q"
 access_secret = "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"
 region = "us-east-1"
-middle_queue = "https://sqs.us-east-1.amazonaws.com/083630338242/DeepSightMid"
 return_queue = "https://sqs.us-east-1.amazonaws.com/083630338242/DeepSightReturns"
 
 sqs = boto3.client('sqs', aws_access_key_id=access_key, aws_secret_access_key=access_secret, region_name=region)
